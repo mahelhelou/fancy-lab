48e3dcd0992fd44cc10b7e8c312ea0ed13cd7fa5
Mahmoud El Helou <mahelhelou@gmail.com>
2022-05-13 01:14:58 +0300

    Apply gulp packages and tasks on the design

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,10 +11,10 @@ function fancy_lab_assets() {
 	wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Rajdhani:400,500,600,700|Seaweed+Script' );
 
 	// Load styles
-	wp_enqueue_style( 'fancy-lab-style', get_template_diretory_uri() . '/app/dist/styles.css', array(), '1.0' );
+	wp_enqueue_style( 'fancy-lab-styles', get_template_directory_uri() . '/app/dist/styles.css', array(), '1.0' );
 
 	// Load scripts
-	wp_enqueue_script( 'fancy-lab-scripts', get_template_directory_uri() . '/dist/bundled.js', ['jquery'], '1.0', true );
+	wp_enqueue_script( 'fancy-lab-scripts', get_template_directory_uri() . '/app/dist/bundled.js', ['jquery'], '1.0', true );
 
 }
 

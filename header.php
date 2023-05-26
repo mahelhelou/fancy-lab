60fa5cefe9907881c04a31e1109531669b49c033
Mahmoud Elhelou <mahelhelou@gmail.com>
2023-05-27 00:14:58 +0300

    Setting up the project to be converted into Tailwind and Webpack

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
@@ -31,72 +31,81 @@
 					<div class="row">
 						<div class="brand col-12 col-md-3 col-lg-2 text-center text-md-left">
 							<!-- Dynamic/custom logo feature -->
-							<a href="<?php echo esc_url( home_url( '/' ) ); // or site_url() ?>">
-								<?php if ( has_custom_logo() ) {
-                  the_custom_logo();
-                } else { ?>
-								<p><?php bloginfo( 'title' ); ?></p>
-								<span><?php bloginfo( 'description' ); ?></span>
+							<a href="<?php echo esc_url(home_url('/')); // or site_url() ?>">
+								<?php if (has_custom_logo()) {
+									the_custom_logo();
+								} else { ?>
+									<p>
+										<?php bloginfo('title'); ?>
+									</p>
+									<span>
+										<?php bloginfo('description'); ?>
+									</span>
 								<?php }
-              ?>
+								?>
 							</a>
 						</div>
 						<div class="second-column col-12 col-md-9 col-lg-10">
 							<div class="row">
-								<?php if ( class_exists( 'WooCommerce' ) ) { ?>
-								<div class="account col-12">
-									<div class="navbar-expand">
-										<ul class="navbar-nav float-left">
-											<?php if ( is_user_logged_in() ) { ?>
-											<li>
-												<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"
-													class="nav-link"><?php esc_html_e( 'My Account', 'fancy-lab' ); ?></a>
-											</li>
-											<li>
-												<a href="<?php echo esc_url( wp_logout_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ) ); ?>"
-													class="nav-link"><?php esc_html_e( 'Logout', 'fancy-lab' ); ?></a>
-											</li>
-											<!-- To allow registration in My account page, go to WooCommerce -> Settings -> Accounts and privacy -> `Allow customers to create an account on the "My account" page` -->
-											<?php } else { ?>
-											<li>
-												<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ); ?>"
-													class="nav-link"><?php esc_html_e( 'Login / Register', 'fancy-lab' ); ?></a>
-											</li>
-											<?php }
-											?>
-										</ul>
-									</div>
+								<?php if (class_exists('WooCommerce')) { ?>
+									<div class="account col-12">
+										<div class="navbar-expand">
+											<ul class="navbar-nav float-left">
+												<?php if (is_user_logged_in()) { ?>
+													<li>
+														<a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))); ?>"
+															class="nav-link"><?php esc_html_e('My Account', 'fancy-lab'); ?></a>
+													</li>
+													<li>
+														<a href="<?php echo esc_url(wp_logout_url(get_permalink(get_option('woocommerce_myaccount_page_id')))); ?>"
+															class="nav-link"><?php esc_html_e('Logout', 'fancy-lab'); ?></a>
+													</li>
+													<!-- To allow registration in My account page, go to WooCommerce -> Settings -> Accounts and privacy -> `Allow customers to create an account on the "My account" page` -->
+												<?php } else { ?>
+													<li>
+														<a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))); ?>"
+															class="nav-link"><?php esc_html_e('Login / Register', 'fancy-lab'); ?></a>
+													</li>
+												<?php }
+												?>
+											</ul>
+										</div>
 
-									<!-- Adding shopping cart to the website -->
-									<div class="cart text-right">
-										<a href="<?php echo esc_url( wc_get_cart_url() ); ?>"><span class="cart-icon"></span></a>
-										<span class="items"><?php echo esc_html( WC()->cart->get_cart_contents_count() ); ?></span>
+										<!-- Adding shopping cart to the website -->
+										<div class="cart text-right">
+											<a href="<?php echo esc_url(wc_get_cart_url()); ?>"><span class="cart-icon"></span></a>
+											<span class="items">
+												<?php echo esc_html(WC()->cart->get_cart_contents_count()); ?>
+											</span>
+										</div>
 									</div>
-								</div>
 								<?php }
-            ?>
+								?>
 								<div class="col-12">
 									<!-- Copied from navwalker github documentation -->
 									<nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
 										<!-- Brand and toggle get grouped for better mobile display -->
 										<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse"
 											data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" `
-											aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'fancy-lab' ); ?>">
+											aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'fancy-lab'); ?>">
 											<span class="navbar-toggler-icon"></span>
 										</button>
 										<!-- <a class="navbar-brand" href="#">Navbar</a> -->
 										<?php
-									wp_nav_menu( array(
-											'theme_location'    => 'fancy_lab_main_menu',
-											'depth'             => 3, // (sub sub menu) Number of menu levels
-											'container'         => 'div',
-											'container_class'   => 'collapse navbar-collapse',
-											'container_id'      => 'bs-example-navbar-collapse-1',
-											'menu_class'        => 'nav navbar-nav',
-											'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-											'walker'            => new WP_Bootstrap_Navwalker(),
-									) );
-									?>
+										wp_nav_menu(
+											array(
+												'theme_location' => 'fancy_lab_main_menu',
+												'depth' => 3,
+												// (sub sub menu) Number of menu levels
+												'container' => 'div',
+												'container_class' => 'collapse navbar-collapse',
+												'container_id' => 'bs-example-navbar-collapse-1',
+												'menu_class' => 'nav navbar-nav',
+												'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+												'walker' => new WP_Bootstrap_Navwalker(),
+											)
+										);
+										?>
 									</nav>
 								</div>
 							</div>
--- searchform.php
+++ searchform.php
@@ -6,22 +6,21 @@
  */
 ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <input type="search" class="search-field"
-    placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'fancy-lab' ); ?>"
-    value="<?php echo get_search_query(); ?>" name="s" />
-  <button type="submit" class="search-submit">
-    <span class="screen-reader-text">
-      <?php echo _x( 'Search', 'submit button', 'fancy-lab' ); ?>
-    </span>
-  </button>
-  <!-- Because we want to distribute the theme and we can use it without woocommerce plugin -->
-  <?php
-    // Make search feature works for every theme, not for woocommerce only
-    if ( class_exists( 'WooCommerce' ) ) { ?>
-  <!-- Because it's woocommerce store, include only products in the search results -->
-  <!-- Before this, the template file was index.php, NOW the template file is archive-product.php -->
-  <input type="hidden" value="product" name="post_type" id="post_type">
-  <?php }
-  ?>
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+	<input type="search" class="search-field"
+		placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'fancy-lab'); ?>"
+		value="<?php echo get_search_query(); ?>" name="s" />
+	<button type="submit" class="search-submit">
+		<span class="screen-reader-text">
+			<?php echo _x('Search', 'submit button', 'fancy-lab'); ?>
+		</span>
+	</button>
+	<!-- Because we want to distribute the theme and we can use it without woocommerce plugin -->
+	<?php
+	// Make search feature works for every theme, not for woocommerce only
+	if (class_exists('WooCommerce')) { ?>
+		<!-- Because it's woocommerce store, include only products in the search results -->
+		<!-- Before this, the template file was index.php, NOW the template file is archive-product.php -->
+		<input type="hidden" value="product" name="post_type" id="post_type">
+	<?php } ?>
 </form>
